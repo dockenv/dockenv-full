e5ec95ec3b592cd96df2c7cbfbc4ea63d4623f47
imxieke <oss@live.hk>
2023-12-05 00:01:21 +0800

    update base image

---
+++ .devcontainer/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+###
+ # @Author: Cloudflying
+ # @Date: 2023-12-04 15:10:23
+ # @LastEditTime: 2023-12-04 15:10:24
+ # @LastEditors: Cloudflying
+ # @Description: Init CodeSpace/CodeSandBox
+### 
+
+# TODO demo rust
